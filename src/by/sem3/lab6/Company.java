ab35a1adc9e787559039a866b4a18f88fa1069e1
TTOJlTOPA <zhuk.valeriy.98@gmail.com>
2016-09-22 21:55:49 +0300

    -

---
+++ src/by/sem3/lab6/Company.java
@@ -0,0 +1,46 @@
+package by.sem3.lab6;
+
+import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by Валерий on 22.09.2016.
+ */
+class Company {
+    String name;
+    String shortTitle;
+    Date dateUpdate;
+    String adress;
+    Date dateFoundation;
+    int countEmployees;
+    String auditor;
+    String phone;
+    String email;
+    String branch;
+    String activity;
+    String link;
+
+    Company() {
+        name = "-";
+        shortTitle = "-";
+        dateUpdate = new Date(0, 0, 0);
+        adress = "-";
+        dateFoundation = new Date(0, 0, 0);
+        countEmployees = 0;
+        auditor = "-";
+        phone = "-";
+        email = "-";
+        branch = "-";
+        activity = "-";
+        link = "-";
+    }
+
+    //Company(String[] param) {}
+
+    private boolean isName(String name) {
+        Pattern pattern = Pattern.compile("^[a-zA-Z]+$");
+        Matcher matcher = pattern.matcher(name);
+        return matcher.matches();
+    }
+}
---
+++ src/by/sem3/lab6/Main.java
@@ -0,0 +1,10 @@
+package by.sem3.lab6;
+
+/**
+ * Created by Валерий on 22.09.2016.
+ */
+public class Main {
+    public static void main(String[] args){
+
+    }
+}
