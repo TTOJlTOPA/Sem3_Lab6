611d31e9fbf74e5f21e77b7d57f0986d47faf4b8
TTOJlTOPA <zhuk.valeriy.98@gmail.com>
2016-09-24 17:25:08 +0300

    2

--- src/by/sem3/lab6/Company.java
+++ src/by/sem3/lab6/Company.java
@@ -34,12 +34,29 @@ class Company {
     }
 
     Company(String[] param) {
-
+        name = param[0];
+        shortTitle = param[1];
+        dateUpdate = toDate(param[2]);
+        adress = param[3];
+        dateFoundation = toDate(param[4]);
+        countEmployees = Integer.parseInt(param[5]);
+        auditor = param[6];
+        phone = param[7];
+        email = param[8];
+        branch = param[9];
+        activity = param[10];
+        link = param[11];
     }
 
     private boolean isName(String name) {
-        Pattern pattern = Pattern.compile("^[a-zA-Z]+$");
+        Pattern pattern = Pattern.compile("[a-zA-Z]+");
         Matcher matcher = pattern.matcher(name);
         return matcher.matches();
+    }
+
+    private Date toDate(String date) {
+        String[] dateParam;
+        dateParam = date.split("[:]");
+        return new Date(Integer.parseInt(dateParam[2]), Integer.parseInt(dateParam[1]), Integer.parseInt(dateParam[0]));
     }
 }
--- src/by/sem3/lab6/Main.java
+++ src/by/sem3/lab6/Main.java
@@ -1,7 +1,32 @@
 package by.sem3.lab6;
 
-public class Main {
-    public static void main(String[] args){
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    private String[] nameOfColumns(String path) throws IOException{
+        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(path)));
+        return reader.readLine().split(";");
+    }
+
+    private List<Company> readFromCSV(String path) throws IOException {
+        List<Company> listOfCompanies = new ArrayList<>();
+        String[] parameters;
+        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(path)));
+        String line;
+        reader.readLine();
+        while (!(line = reader.readLine()).isEmpty()) {
+            parameters = line.split(";");
+            listOfCompanies.add(new Company(parameters));
+        }
+        return listOfCompanies;
     }
 }
